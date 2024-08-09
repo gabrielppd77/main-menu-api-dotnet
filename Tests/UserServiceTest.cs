4d61bdfd1f2ac4ff9307b2cb7f6e509597d79353
Gabriel Domingos <gabrielppd77@outlook.com>
2024-08-08 22:20:27 -0300

    add unit tests in project

---
+++ Tests/UserServiceTest.cs
@@ -0,0 +1,32 @@
+using main_menu.Database.repositories;
+using main_menu.DTOS;
+using main_menu.Services;
+using Moq;
+using Xunit;
+
+namespace main_menu.Tests
+{
+	public class UserServiceTest
+	{
+		[Fact]
+		public async Task CreateUser()
+		{
+			var repository = new Mock<UserRepository>();
+			var service = new UserService(repository.Object);
+
+			var data = new RegistrationRequestDTO()
+			{
+				Email = "fake-email@email.com",
+				Password = "123456",
+			};
+
+			var newUser = await service.CreateUser(data);
+
+			Assert.NotNull(newUser);
+			Assert.NotEqual(data.Password, newUser.Password);
+
+			//user esteje no banco de dados
+			//user seje retornado
+		}
+	}
+}
