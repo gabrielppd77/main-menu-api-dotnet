44cb5db92ad4c902156a928dad57d316fbbb74aa
Gabriel Domingos <gabrielppd77@outlook.com>
2024-09-04 21:09:13 -0300

    fix types in dtos

--- DTOS/ClientDTOS/ClientProductResponseDTO.cs
+++ DTOS/ClientDTOS/ClientProductResponseDTO.cs
@@ -7,7 +7,7 @@ namespace main_menu.DTOS.ClientDTOS
 		public Guid Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
-		public string ImageUrl { get; set; }
+		public string? ImageUrl { get; set; }
 		public int Order { get; set; }
 		public decimal Price { get; set; }
 
--- DTOS/ProductDTOS/ProductResponseDTO.cs
+++ DTOS/ProductDTOS/ProductResponseDTO.cs
@@ -8,7 +8,7 @@ namespace main_menu.DTOS.ProductDTO
 		public Guid Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
-		public string ImageUrl { get; set; }
+		public string? ImageUrl { get; set; }
 		public int Order { get; set; }
 		public decimal Price { get; set; }
 		public Guid CategoryId { get; set; }
